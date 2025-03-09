477bd34617a8c326ccc0b4a8b9771ac2050b2573
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-03-09 23:03:57 +0530

    [Rakshit] Add: footer component ui

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='w-full bg-[#2E1D1E] h-[50px]'>
+            <div className='max-w-6xl h-full flex text-xs items-center mx-auto  text-white'>
+                <p>Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+
 }
 
 export default Footer
