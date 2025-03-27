e7b957bc52de5f933001b6f46adba548be8e2d0a
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-03-27 21:51:40 +0530

    [Rakshit] Add: unit test cases for header component

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom"; 
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { MemoryRouter } from "react-router-dom";
 import Header from "../components/Common/Header";
 import React from "react";
+import SearchProvider from "../context/SearchProvider";
 
 const mockStore = configureStore([]);
 const store = mockStore({
@@ -14,6 +15,7 @@ const store = mockStore({
 });
 
 describe("Header Component Tests", () => {
+
   test("should render properly", () => {
     render(
       <Provider store={store}>
@@ -23,8 +25,46 @@ describe("Header Component Tests", () => {
       </Provider>
     );
 
-
     const bookStoreText = screen.getByText(/bookstore/i);
     expect(bookStoreText).toBeInTheDocument();
   });
+
+
+  test("should not render search and cart icons if container is not provided", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    const searchBar = screen.queryByPlaceholderText(/search.../i)
+    const cart = screen.queryByText(/cart/i)
+    const profile = screen.queryByText(/profile/i)
+
+    expect(searchBar).not.toBeInTheDocument()
+    expect(cart).not.toBeInTheDocument()
+    expect(profile).not.toBeInTheDocument()
+  })
+
+  test("search bar change event", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <SearchProvider>
+            <Header container="home" />
+          </SearchProvider>
+        </MemoryRouter>
+      </Provider>
+    )
+
+    const searchInput = screen.getByPlaceholderText(/search/i)
+
+    fireEvent.change(searchInput, { target: { value: "random value" } })
+
+    expect(searchInput).toHaveValue('random value')
+  })
+
+
 });
