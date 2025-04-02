91c8f473d7a29f9782af3a34eaca64b8401eead8
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-02 11:39:13 +0530

    [Rakshit] Refactor: added unit test cases for Header

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
@@ -7,12 +7,29 @@ import Header from "../components/Common/Header";
 import React from "react";
 import SearchProvider from "../context/SearchProvider";
 
+jest.mock("../components/Common/ProfileDropdown", () => (props: any) => {
+  return (
+    <div data-testid="profile-dropdown">
+      <button onClick={props.logout} data-testid="logout-button">Logout</button>
+    </div>
+  );
+});
+
+
 const mockStore = configureStore([]);
 const store = mockStore({
   cart: { cart: [] },
   wishList: { wishList: [] },
   user: { isAuthenticated: false },
 });
+store.dispatch = jest.fn();
+
+jest.mock("react-toastify", () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn()
+  }
+}))
 
 describe("Header Component Tests", () => {
 
@@ -66,5 +83,31 @@ describe("Header Component Tests", () => {
     expect(searchInput).toHaveValue('random value')
   })
 
+  test("user should be logged out when clicking on logout button", async () => {
+    localStorage.setItem("token", "test_token");
 
-});
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <SearchProvider>
+            <Header container="home" />
+          </SearchProvider>
+        </MemoryRouter>
+      </Provider>
+    );
+
+
+    const profileButton = screen.getByTestId("profile-dropdown");
+    fireEvent.click(profileButton);
+
+
+    const logoutButton = await screen.findByTestId("logout-button");
+    fireEvent.click(logoutButton);
+
+    waitFor(() => {
+      expect(localStorage.removeItem).toHaveBeenCalledWith("token");
+      expect(localStorage.getItem("token")).toBeNull();
+      expect(store.dispatch).toHaveBeenCalledTimes(2);
+    });
+  })
+})
